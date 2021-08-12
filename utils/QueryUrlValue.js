e9c498051aad001dc3e5654ab0fb8124bb726333
Frankie <1426203851@qq.com>
2021-08-13 01:55:13 +0800

    feat: 新增获取 URL 参数的方法

---
+++ utils/QueryUrlValue.js
@@ -0,0 +1,23 @@
+/**
+ * 获取 URL 上的某参数值
+ *
+ * @param {*} key 参数名称
+ * @returns 参数值
+ */
+const queryUrlValue = key => {
+  if (!key) return ''
+
+  const url = decodeURIComponent(window.location.href)
+  const re = new RegExp(`[?|&]${key}=([^&]+)`, 'g')
+  const matchResult = re.exec(url)
+
+  if (!matchResult) return ''
+
+  let value = matchResult[1]
+  if (value.includes('#')) {
+    const separator = value.includes('/#') ? '/#' : '#'
+    value = value.split(separator)[0]
+  }
+
+  return value
+}
