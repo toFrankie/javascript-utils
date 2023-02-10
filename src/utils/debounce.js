31ca09fa2bc37cffa1b13a2b26ef3c27b3845065
Frankie <1426203851@qq.com>
2023-02-10 11:41:59 +0800

    fix: typo

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -4,15 +4,15 @@
  * 函数防抖
  * @param {Function} func 要防抖的函数
  * @param {number} wait 需要延迟的毫秒数
- * @param {boolean} immdeiate 是否立即执行
+ * @param {boolean} immediate 是否立即执行
  * @returns {Function} 返回新的 debounced（防抖动）函数
  */
-export const debounce = (func, wait = 0, immdeiate = false) => {
+export const debounce = (func, wait = 0, immediate = false) => {
   let timerId
   return function (...args) {
     if (timerId) clearTimeout(timerId)
 
-    if (immdeiate && !timerId) {
+    if (immediate && !timerId) {
       func.apply(this, args)
     }
 
